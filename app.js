26110d149197daa01fade9c70d90341583d2f1df
KhacTam <tamb1906562@student.ctu.edu.vn>
2022-09-21 09:53:12 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const { BadRequestError, errorHandler } = require("./app/api-error");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-// const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+  });
+  
+app.use((err, req, res, next) => {
+    errorHandler.handleError(err, res);
+  });
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
